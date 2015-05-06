ef49db04bcf1d5337949d1003cadb9123a8eedc4
mogh <mogh@example.com>
2015-05-06 11:59:31 -0400

    final touches

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   #PUT BAKING LIST AND SHIPPING LIST DASHBOARD HERE OR CHANGE WHERE THEY REDIRECT TO
 
   def home
+    @unshipped_orders_backwards = Order.chronological.not_shipped
     @unshipped_orders = Order.chronological.not_shipped.reverse_each
     @muffins_baking_list = create_baking_list_for("muffins")
     @bread_baking_list = create_baking_list_for("bread")
